db098e471db4479076c0855ec9250087e98fb9af
zy_H <hzy18023@gmail.com>
2022-08-29 21:32:22 +0900

    chore: unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        Components({
+            dirs: ['src/components'],
+            deep: true,
+        }),
         AutoImport({
             include: [
                 /\.[tj]sx?$/,
