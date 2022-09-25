2298fd78c41eed31bd991a22aff690acebef7470
zy_H <hzy18023@gmail.com>
2022-09-25 21:36:46 +0900

    chore: remove UnoCSS, add TailwindCSS

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import 'uno.css'
 import { createApp } from 'vue'
 import App           from './App.vue'
 import { router }    from './router'
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
-import vue                              from '@vitejs/plugin-vue'
-import { presetAttributify, presetUno } from 'unocss'
-import Unocss                           from 'unocss/vite'
-import AutoImport                       from 'unplugin-auto-import/vite'
-import IconsResolver                    from 'unplugin-icons/resolver'
-import Icons                            from 'unplugin-icons/vite'
-import Components                       from 'unplugin-vue-components/vite'
-import { defineConfig }                 from 'vite'
+import vue              from '@vitejs/plugin-vue'
+import AutoImport       from 'unplugin-auto-import/vite'
+import IconsResolver    from 'unplugin-icons/resolver'
+import Icons            from 'unplugin-icons/vite'
+import Components       from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        Unocss({
-            presets: [
-                presetAttributify({}),
-                presetUno(),
-            ],
-        }),
         Icons({
             scale: 2, // Scale of icons against 1em
             defaultStyle: '', // Style apply to icons
