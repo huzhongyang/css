bc5ebade37813c86735c485b8d9afe265d5ab645
zy_H <hzy18023@gmail.com>
2022-09-25 17:23:36 +0900

    chore: support UnoCSS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import 'uno.css'
 import { createApp } from 'vue'
-import App from './App.vue'
-import { router } from './router'
+import App           from './App.vue'
+import { router }    from './router'
 import './style.css'
 
 createApp(App).use(router).mount('#app')
