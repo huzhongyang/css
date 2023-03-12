cc1605d08eb03537bca72c7b5e650b382a5dda09
zyH <hzy18023@gmail.com>
2023-03-12 21:41:28 +0800

    feat: search city and show

--- src/components/23-03-12/ZWeather.vue
+++ src/components/23-03-12/ZWeather.vue
@@ -1,14 +1,13 @@
 <script lang='ts' setup>
   import { computed } from 'vue'
   import { $ref } from 'vue/macros'
+  import { City, getCitys } from '../../api'
 
   let searchCity = $ref('')
-  const clearInputHidden = computed(() => {
-    return searchCity === ''
-  })
+  const clearInputHidden = computed(() => searchCity === '')
   const searchCityRef = $ref<HTMLInputElement>()
-  let citys = $ref<string[]>()
-  let citysDivVisited = $ref(false)
+  let citys = $ref<City[]>([])
+  let citysDivVisited = computed(() => citys?.length !== 0)
 
   function getWeather() {
     console.log(searchCity)
@@ -16,16 +15,20 @@
 
   function clearInput() {
     searchCity = ''
+    citys = []
     searchCityRef!.focus()
   }
 
   onMounted(() => {
     searchCityRef!.focus()
+    searchCityRef!.addEventListener('change', async () => {
+      citys = await getCitys(searchCity) ?? []
+    })
   })
 </script>
 
 <template>
-  <div class="w-full h-full flex justify-center items-center flex-col">
+  <div class="w-full h-full flex items-center flex-col">
     <h1>查询城市天气</h1>
     <div class="flex">
       <div class="relative flex items-center">
@@ -42,8 +45,10 @@
               class="bg-blue-200 h-8 px-2 py-1 rounded ml-2">查询
       </button>
     </div>
-    <div v-if="citysDivVisited">
-      <span v-for="city in citys">{{ city }}</span>
+    <div v-show="citysDivVisited"
+         class="relative top-1 left-[-28px] w-[250px] h-[200px] overflow-scroll flex flex-col
+                border border-solid p-1.5 rounded">
+      <span v-for="city in citys">{{ city.country + ' - ' + city.name }}</span>
     </div>
   </div>
 </template>
